3ec6f553b1fcd9d7545d66fc1799e5d6c49a2456
Udara Damitha <udaradamitha@gmail.com>
2025-05-04 17:55:45 +0530

    FrameworkDetailView

---
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,54 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by udara on 2025-05-04.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var framework: Framework
+    @State private var isShowingSafariView = false
+    
+    var body: some View {
+        VStack {
+            
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                isShowingSafariView = true
+            } label: {
+                Label("Learn More", systemImage: "book.fill")
+            }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            .tint(.red)
+        }
+    }
+}
+
+#Preview {
+    FrameworkDetailView(framework: MockData.sampleFramework)
+        .preferredColorScheme(.dark)
+}
