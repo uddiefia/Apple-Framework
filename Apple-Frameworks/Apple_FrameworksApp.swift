f064a365a556e45ef7271e325f8e99e544beef88
Udara Damitha <udaradamitha@gmail.com>
2025-05-04 15:53:41 +0530

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by udara on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
