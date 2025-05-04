0d5ee0a87317acd8f797428829446dbcf01a01a8
Udara Damitha <udaradamitha@gmail.com>
2025-05-04 17:06:09 +0530

    NavigationView

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -12,41 +12,36 @@ struct FrameworkGridView: View {
     let columns : [GridItem] = [GridItem(.flexible()), GridItem(.flexible()),GridItem(.flexible())]
     
     var body: some View {
-        LazyVGrid(columns: columns){
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clip", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns){
+                    ForEach(MockData.frameworks, id: \.id) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            
+            .navigationTitle("Frameworks")
         }
         
     }
 }
 
 #Preview {
-    FrameworkGridView()
+    FrameworkGridView().preferredColorScheme(.dark)
 }
 
 struct FrameworkTitleView: View {
-    let name :String
-    let imageName : String
-    
+    let framework :Framework
     var body: some View {
         VStack{
-            Image(imageName).resizable()
+            Image(framework.imageName).resizable()
                 .frame(width: 90, height: 90)
             
-            Text(name).font(.title2)
+            Text(framework.name).font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
-        }
+        }.padding()
     }
 }
